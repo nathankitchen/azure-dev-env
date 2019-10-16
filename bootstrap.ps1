a936013a78862086f45ec6360dfe3718a31b4fc1
Nathan Kitchen <github@nathankitchen.com>
2019-10-16 23:51:50 +0100

    Install more

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -3,3 +3,15 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
 
 choco install git.commandline -y;
+
+Set-Item -Path Env:Path -Value ($Env:Path + ";C:\tools\git")
+
+choco install docker-desktop;
+
+choco install dotnetcore-sdk --version=2.2.402;
+
+choco install vscode;
+choco install vscode-yaml;
+choco install vscode-powershell;
+choco install vscode-csharp;
+choco install vscode-docker;
